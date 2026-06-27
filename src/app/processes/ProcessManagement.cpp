bbfade04705f846d5353ed07110a1d69ee2d3390
sahilgupta630 <guptasahilkumar783@gmail.com>
2026-06-28 01:44:51 +0530

    feat(processes): implement parallel execution with process synchronization logic

---
+++ src/app/processes/ProcessManagement.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "ProcessManagement.hpp"
+#include <unistd.h>
+#include <cstring>
+#include <sys/wait.h>
+#include "../encryptDecrypt/Cryption.hpp"
+
+ProcessManagement::ProcessManagement() : childrenCount(0) {}
+
+bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task) {
+    taskQueue.push(std::move(task));
+    int pid = fork();
+    if(pid < 0){
+        return false;
+    }else if(pid > 0){
+        std::cout<<"Entering the parent process"<<std::endl;
+        childrenCount++;
+    }else{
+        std::cout<<"Entering the child process"<<std::endl; 
+        executeTask();
+        std::cout<<"Exiting the child process"<<std::endl;
+    }
+    return true;
+}
+
+void ProcessManagement::executeTask() {
+    
+    if(!taskQueue.empty()){
+        std::unique_ptr<Task> task = std::move(taskQueue.front());
+        taskQueue.pop();
+        std::cout<<"Executing task: "<<task->toString()<<std::endl;
+        executeCryption(task->toString());
+    }
+
+}
+
+void ProcessManagement::waitForAllProcesses() {
+    int status;
+    for (int i = 0; i < childrenCount; ++i) {
+        wait(&status);
+    }
+    std::cout << "All background processes finished." << std::endl;
+}
